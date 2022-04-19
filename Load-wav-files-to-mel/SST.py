0830a66cfaa8e1dd511b330801b37c4a305493e0
Cesar Chen <cesarchen616@outlook.com>
2022-04-19 23:53:32 +0800

    Day27

--- Load-wav-files-to-mel/SST.py
+++ Load-wav-files-to-mel/SST.py
@@ -7,6 +7,18 @@ from Mel_spectrum import *
 
 time_start = time.time()  # time = 0
 ########################################################################
+def CWT(path):
+    y, sr = librosa.load(path, sr=None)
+    t = np.linspace(0, 10, sr, endpoint=False)
+    Twy, Wy, *_ = ssq_cwt(y)
+
+    plt.figure(figsize=(10, 4))
+    #plt.colorbar(format='%+2.0f dB')
+    plt.title('CWT')
+    plt.tight_layout()
+    plt.imshow(np.abs(Wy), aspect='auto',  vmin=0, vmax=.001,cmap='turbo')
+    plt.show()
+
 def SST(path):
     y, sr = librosa.load(path, sr=None)
     t = np.linspace(0, 10, sr, endpoint=False)
@@ -16,9 +28,10 @@ def SST(path):
     #plt.colorbar(format='%+2.0f dB')
     plt.title('SST')
     plt.tight_layout()
-    plt.imshow(np.abs(Wy), aspect='auto', cmap='turbo')
+    plt.imshow(np.abs(Twy), aspect='auto', vmin=0, vmax=.001, cmap='turbo')
     plt.show()
 
+CWT("vavle.00.normal.00000023.wav")
 SST("vavle.00.normal.00000023.wav")
 
 def viz(x, Tx, Wx):
@@ -26,7 +39,6 @@ def viz(x, Tx, Wx):
     plt.show()
     plt.imshow(np.abs(Tx), aspect='auto', vmin=0, vmax=.2, cmap='turbo')
     plt.show()
-
 
 ########################################################################
 time_end = time.time()
